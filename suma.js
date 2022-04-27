3d7644741495161ac86d1a3aa1123260a0041aeb
JacoboPetrelli <92336672+JacoboPetrelli@users.noreply.github.com>
2022-04-26 21:42:54 -0500

    Add files via upload

--- suma.js
+++ suma.js
@@ -1,9 +1,9 @@
 let suma = 0
-i = 1
+numerador = 1
 for(let k = 0; k <= 1001; k++){
 let denominador = 2*k+1;    
-suma = suma + (i/denominador)
-i = i *-1
+suma = suma + (numerador/denominador)
+numerador = numerador *-1
 }
 suma = 4*(suma)
 console.log("La suma es igual a " + suma)
